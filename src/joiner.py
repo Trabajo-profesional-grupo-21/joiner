98c9401a2a67a23001b2264a19449a8e946c1374
SaFernandezC <SFernandezC@fi.uba.ar>
2024-05-16 19:36:42 -0300

    feat: support for images

--- src/joiner.py
+++ src/joiner.py
@@ -21,6 +21,7 @@ class Joiner():
         self.current_batches_arousal = {}
         self.current_batches_valence = {}
         self.current_batches = {}
+        self.current_images = {}
         self.last_amount = {}
 
         self.init_conn()
@@ -70,6 +71,56 @@ class Joiner():
             self.last_amount.pop(user, None)
             self.last_batches.pop(user, None)
 
+    def process_income(self, body):
+        origin = body['origin']
+        batch_id = body['batch_id'] 
+        output = {}
+        current = self.current_batches_valence
+        other = self.current_batches_arousal
+        if origin == "arousal":
+            current = self.current_batches_arousal
+            other = self.current_batches_valence
+
+        complete_batch = False
+        if batch_id in other.keys():
+            current_arousal = other[batch_id]
+            arousal_replies = current_arousal['replies']
+            merged_replies = {}
+            for key, value in arousal_replies.items():
+                if not value: # OpenFace no detecto caras y devuelve None
+                    value = {}
+                merged_frame_info = value.copy()
+                merged_frame_info.update(body['replies'][key])
+                merged_replies[key] = merged_frame_info
+            output["user_id"] = body["user_id"]
+            output["batch_id"] = body["batch_id"]
+            output["replies"] = merged_replies
+            complete_batch = True
+            del other[batch_id]
+        else: 
+            del body['origin']
+            current[batch_id] = body
+        
+        return output, complete_batch
+
+    def process_image(self, body):
+        origin = body['origin']
+        image_id = body['img_name']
+        user = body["user_id"]
+        data = body["reply"]
+        del body['origin']
+
+        current_image = self.current_images.get(image_id, {})
+        current_image[origin] = data
+        self.current_images[image_id] = current_image
+        
+        if len(current_image) == 2:
+            reply = {"user_id": user, "img_name": image_id, "batch": current_image}
+            self.redis.rpush(f'{user}-{image_id}', json.dumps(reply))
+            self.current_images.pop(image_id)
+        
+        return None, False
+
     def _check_batch(self, body: dict):
         # puede ser de arousal o valencia
         body = json.loads(body.decode())
@@ -80,55 +131,10 @@ class Joiner():
             self.last_amount[user] = amount
             self.remove_user(user)
             return None, False
-
-        origin = body['origin']
-        batch_id = body['batch_id'] 
-        output = {}
-        complete_batch = False
-        # logging.info(f"Origin {origin}")
-        if origin == "valence":
-            if batch_id in self.current_batches_arousal.keys():
-                #logging.info(f"Tengo arousal y valencia del batch {batch_id}")
-                #logging.info(f"keys del arousal { self.current_batches_arousal}")
-                current_arousal = self.current_batches_arousal[batch_id]
-                arousal_replies = current_arousal['replies']
-                merged_replies = {}
-                for key, value in arousal_replies.items():
-                    merged_frame_info = value.copy()
-                    #logging.info(f"busco key {key} en body {body['replies'].keys()}") 
-                    merged_frame_info.update(body['replies'][key])
-                    merged_replies[key] = merged_frame_info
-                #logging.info(f"merge info {merged_replies}")
-                output["user_id"] = body["user_id"]
-                output["batch_id"] = body["batch_id"]
-                output["replies"] = merged_replies
-                complete_batch = True
-                del self.current_batches_arousal[batch_id]
-            else: 
-                del body['origin']
-                self.current_batches_valence[batch_id] = body
-        elif origin == "arousal":
-            if batch_id in self.current_batches_valence.keys():
-                #logging.info(f"Tengo arousal y valencia del batch {batch_id}")
-                #logging.info(f"keys del valencia { self.current_batches_valence}")
-                current_valence = self.current_batches_valence[batch_id]
-                valence_replies = current_valence['replies']
-                merged_replies = {}
-                for key, value in valence_replies.items():
-                    merged_frame_info = value.copy()
-                    # logging.info(f"busco key {key} en body {body['replies'].keys()}") 
-                    merged_frame_info.update(body['replies'][key])
-                    merged_replies[key] = merged_frame_info
-                #logging.info(f"merge info {merged_replies}")
-                output["user_id"] = body["user_id"]
-                output["batch_id"] = body["batch_id"]
-                output["replies"] = merged_replies
-                complete_batch = True
-                del self.current_batches_valence[batch_id]
-            else: 
-                del body['origin']
-                self.current_batches_arousal[batch_id] = body        
-        return output, complete_batch
+        elif "img_name" in body:
+            return self.process_image(body)
+        else:
+            return self.process_income(body)
 
     def _callback(self, body: dict, ack_tag):
         batch, complete_batch = self._check_batch(body)
